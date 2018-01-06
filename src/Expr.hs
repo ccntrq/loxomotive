1eeb10ed255017669421402fe539bee4fe81570b
Alex Pankoff <ccntrq@screenri.de>
2018-01-06 20:41:59 +0100

    add 'Expr' and 'Stmt'

---
+++ src/Expr.hs
@@ -0,0 +1,19 @@
+module Expr where
+
+import Object
+import Token
+
+data Expr
+    = Assign Token Expr
+    | Binary Expr Token Expr
+    | Call Expr Token [Expr] -- The opening args paren is saved for errors
+    | Get Expr Token
+    | Grouping Expr
+    | Literal Object
+    | Logical Expr Token Expr
+    | Set Object Token Expr
+    | Super Token Token
+    | This Token
+    | Unary Token Expr
+    | Variable Token
+    deriving (Show)
---
+++ src/Stmt.hs
@@ -0,0 +1,16 @@
+module Stmt where
+
+import Expr
+import Token
+
+data Stmt
+    = Block [Stmt]
+    | Class Token Expr [Stmt]
+    | Expression Expr
+    | Function Token [Token] [Stmt]
+    | If Expr Stmt Stmt
+    | Print Expr
+    | Return Token Expr
+    | Var Token Expr
+    | While Expr Stmt
+    deriving (Show)
