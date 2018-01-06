c64c00b82091a3594fdc106eec68072597f03cac
Alex Pankoff <ccntrq@screenri.de>
2018-01-06 17:33:10 +0100

    Lox: expect filename as arg and execute the source at the given location

--- Lox.hs
+++ Lox.hs
@@ -2,5 +2,13 @@ module Lox where
 
 import Scanner
 
+import System.Environment
+
 main :: IO ()
-main = print $ runScanner (initState "()") scanTokens
+main = getArgs >>= \args -> execFile $ head args
+
+execFile :: String -> IO ()
+execFile path = readFile path >>= \src -> runSource src
+
+runSource :: String -> IO ()
+runSource src = print $ runScanner (initState src) scanTokens
