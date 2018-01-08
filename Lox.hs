6a62459f083d60e164d2b68e9ac015be604d039a
Alex Pankoff <ccntrq@screenri.de>
2018-01-08 23:23:38 +0100

    Lox: hook up the interpreter with a little repl

--- Lox.hs
+++ Lox.hs
@@ -2,17 +2,52 @@ module Lox where
 
 import Scanner
 import Parser
+import Interpreter
 
+import System.IO
 import System.Environment
+import System.Exit
+
 
 main :: IO ()
-main = getArgs >>= \args -> execFile $ head args
+main = getArgs >>= dispatch
+
+dispatch :: [String] -> IO ()
+dispatch [] = repl
+dispatch files
+    | length files == 1 = execFile $ head files
+    | otherwise = usage
+
+version :: IO ()
+version = putStrLn "loxWork v0.0.1"
+
+usage :: IO ()
+usage = version >> putStrLn "Usage: Lox [filename]"
+
+repl :: IO ()
+repl = do
+  hSetBuffering stdout NoBuffering
+  repl'
+  where
+    repl' = do
+        putStr "> "
+        input <- getLine
+        if input /= "quit"
+            then do
+                runSource input
+                repl
+            else exit
 
 execFile :: String -> IO ()
-execFile path = readFile path >>= \src -> runSource src
+execFile path = readFile path >>= \src -> runSource src >> exit
 
 runSource :: String -> IO ()
 runSource src =
     case scan src of
       Left e -> print e
-      Right ts -> mapM print ts >> print (parseIt ts) >> return ()
+      Right ts -> case parseIt ts of
+          Left e -> print e
+          Right stmts -> interpret stmts
+
+exit :: IO ()
+exit = exitWith ExitSuccess
